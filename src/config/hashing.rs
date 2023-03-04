59648982b032b383e3b1ee36c80c5971594de059
code-ness <jakhongir.dev@gmail.com>
2023-03-04 22:29:47 +0500

    remove hashing password module

--- src/config/hashing.rs
+++
@@ -1,9 +0,0 @@
-use actix_web::http::StatusCode;
-
-pub fn hash_password(password: &str) -> Result<String, StatusCode> {
-    bcrypt::hash(password.to_owned(), 14).map_err(|_err| StatusCode::INTERNAL_SERVER_ERROR)
-}
-
-pub fn verify_password(password: String, hash: &str) -> Result<bool, StatusCode> {
-    bcrypt::verify(password, hash).map_err(|_err| StatusCode::INTERNAL_SERVER_ERROR)
-}
